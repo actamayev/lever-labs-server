8a77809215fd1174ac78124db6f6dceb0e187890
atamayev <75034340+atamayev@users.noreply.github.com>
2024-10-19 22:42:11 -0400

    Update get-allowed-origins.ts

--- src/utils/get-allowed-origins.ts
+++ src/utils/get-allowed-origins.ts
@@ -1,7 +1,7 @@
 export default function allowedOrigins(): string[] {
 	if (process.env.NODE_ENV === "production-dev" || process.env.NODE_ENV === "production-prod" ) {
-		return [ "https://bluedotrobots.com" ]
+		return [ "https://bluedotrobots.com", "ws://dev-api.bluedotrobots.com" ]
 	} else {
-		return [ "http://localhost:3000" ]
+		return [ "http://localhost:3000", "ws://localhost:3000" ]
 	}
 }
