9be95503b7961245cc85e62e7f689fcf8862ddfa
atamayev <75034340+actamayev@users.noreply.github.com>
2024-11-21 13:35:42 -0500

    Cleanup

--- src/index.ts
+++ src/index.ts
@@ -33,7 +33,7 @@ const esp32WSServer = new WSServer({ noServer: true })
 Esp32SocketManager.getInstance(esp32WSServer) // Directly use getInstance with wss
 
 // TODO: Uncomment when ECS setup:
-CompilerContainerManager.getInstance() // Starts up/warms up the container
+// CompilerContainerManager.getInstance() // Starts up/warms up the container
 
 // Handle WebSocket upgrade for ESP32 connections
 httpServer.on("upgrade", (request, socket, head) => {
