2e4d5599d44d2f9a8bc3121688e57e29a63e29aa
Blue Dot Robots <bluedotrobots@gmail.com>
2025-01-04 21:44:26 -0500

    Rename files (#26)
    Co-authored-by: atamayev <75034340+actamayev@users.noreply.github.com>

--- src/controllers/pip/retrieve-pip-uuid-status.ts
+++ src/controllers/pip/retrieve-pip-uuid-status.ts
@@ -3,7 +3,7 @@ import { Response, Request } from "express"
 import findPipUUID from "../../db-operations/read/find/find-pip-uuid"
 import Esp32SocketManager from "../../classes/esp32/esp32-socket-manager"
 
-export default async function isValidPipUUID(req: Request, res: Response): Promise<void> {
+export default async function retrievePipUUIDStatus(req: Request, res: Response): Promise<void> {
 	try {
 		const { pipUUID } = req.params as { pipUUID: PipUUID }
 
@@ -16,11 +16,11 @@ export default async function isValidPipUUID(req: Request, res: Response): Promi
 
 		const pipConnectionStatus = Esp32SocketManager.getInstance().getESPStatus(pipUUID)
 
-		res.status(200).json({ pipName: pipUUIDData.pip_name, pipConnectionStatus})
+		res.status(200).json({ pipName: pipUUIDData.pip_name, pipConnectionStatus })
 		return
 	} catch (error) {
 		console.error(error)
-		res.status(500).json({ error: "Internal Server Error: Unable to confirm Pip UUID is valid" })
+		res.status(500).json({ error: "Internal Server Error: Unable to retrieve PipUUID status" })
 		return
 	}
 }
