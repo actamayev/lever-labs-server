6e437b4fefc773aebf924937b7e649adfe060e55
atamayev <75034340+actamayev@users.noreply.github.com>
2025-01-13 15:59:28 -0500

    Add endpoint

---
+++ src/middleware/request-validation/internal/validate-decode-email-subscriber.ts
@@ -0,0 +1,30 @@
+import Joi from "joi"
+import _ from "lodash"
+import { Request, Response, NextFunction } from "express"
+import Encryptor from "../../../classes/encryptor"
+
+const subscribeForEmailUpdatesSchema = Joi.object({
+	email: Joi.string().required()
+}).required()
+
+export default function validateDecodeEmailSubscriber(req: Request, res: Response, next: NextFunction): void {
+	try {
+		const { error } = subscribeForEmailUpdatesSchema.validate(req.body)
+
+		if (!_.isUndefined(error)) {
+			res.status(400).json({ validationError: error.details[0].message })
+			return
+		}
+
+		if (!Encryptor.isDeterministicEncryptedString(req.body.email)) {
+			res.status(400).json({ validationError: "Email is not encrypted" })
+			return
+		}
+
+		next()
+	} catch (error) {
+		console.error(error)
+		res.status(500).json({ error: "Internal Server Error: Unable to Validate encrypted email" })
+		return
+	}
+}
