74e37dd1db281e3a2410807c9c9510400e6f1487
Ariel Tamayev <actamayev@gmail.com>
2025-01-13 16:08:32 -0500

    Add endpoint (#27)
    * Add endpoint
    
    * Package updates
    
    ---------
    
    Co-authored-by: atamayev <75034340+actamayev@users.noreply.github.com>

--- src/routes/internal-routes.ts
+++ src/routes/internal-routes.ts
@@ -1,9 +1,13 @@
 import express from "express"
 
 import addNewPipUUID from "../controllers/internal/add-new-pip-uuid"
+import decodeEmailSubscriber from "../controllers/internal/decode-email-subscriber"
+import validateDecodeEmailSubscriber from "../middleware/request-validation/internal/validate-decode-email-subscriber"
 
 const internalRoutes = express.Router()
 
 internalRoutes.post("/add-pip-uuid", addNewPipUUID)
+
+internalRoutes.post("/decode-email-subscriber", validateDecodeEmailSubscriber, decodeEmailSubscriber)
 
 export default internalRoutes
