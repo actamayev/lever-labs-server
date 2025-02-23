512fd859a1b797efb2a8992abc092bef2517bdab
atamayev <75034340+actamayev@users.noreply.github.com>
2025-02-22 19:05:22 -0500

    Misc

--- src/controllers/lab-demo/motor-control.ts
+++ src/controllers/lab-demo/motor-control.ts
@@ -1,6 +1,7 @@
 import { Response, Request } from "express"
 import Esp32SocketManager from "../../classes/esp32/esp32-socket-manager"
 
+// TODO: Delete this file
 export default async function motorControl(req: Request, res: Response): Promise<void> {
 	try {
 		const { motorControlData } = req.body as { motorControlData: IncomingMotorControlData }
