b4054208e4af3cf9919a74a1fa8f3a1815b869d1
atamayev <75034340+actamayev@users.noreply.github.com>
2025-02-25 10:47:32 -0500

    Update generate-uuid.ts

--- src/controllers/internal/generate-uuid.ts
+++ src/controllers/internal/generate-uuid.ts
@@ -1,5 +1,6 @@
 import { Response, Request } from "express"
 
+// This is used for generating UUIDs when adding data to the reading_questions.csv and activities.csv
 export default function generateUUID(req: Request, res: Response): void {
 	try {
 		const uuid = crypto.randomUUID()
