fdf5135b42046f7d5f28d6ef66d80c1306c2235d
atamayev <bluedotrobots@gmail.com>
2025-04-18 14:35:41 -0400

    Update bytecode-types.ts

--- src/types/bytecode-types.ts
+++ src/types/bytecode-types.ts
@@ -1,4 +1,3 @@
-/* eslint-disable no-useless-escape */
 export enum BytecodeOpCode {
 	NOP = 0x00,
 	END = 0x01,
@@ -113,7 +112,7 @@ export const CommandPatterns: Record<CommandType, RegExp> = {
 	[CommandType.VARIABLE_ASSIGNMENT]: /^(float|int|bool)\s+(\w+)\s*=\s*(.+)$/,
 
 	// TODO: Have the availability to have a sensor reading on the right-hand side.
-	[CommandType.IF_STATEMENT]: /^if\s*\(\s*(Sensors::getInstance\(\)\.\w+\(\)|[-\d\.]+|\w+)\s*([<>=!][=]?)\s*(-?\d+\.?\d*|\w+)\s*\)$/,
+	[CommandType.IF_STATEMENT]: /^if\s*\(\s*(Sensors::getInstance\(\)\.\w+\(\)|[-\d.]+|\w+)\s*([<>=!][=]?)\s*(-?\d+\.?\d*|\w+)\s*\)$/,
 	[CommandType.ELSE_STATEMENT]: /^else$/,
 	[CommandType.BLOCK_START]: /^{$/,
 	[CommandType.BLOCK_END]: /^}$/,
