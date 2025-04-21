456ec1393ef27ce2e067c7d21a78c689ded72fd6
atamayev <bluedotrobots@gmail.com>
2025-04-21 10:42:10 -0400

    Change file name

--- src/controllers/internal/get-latest-firmware-data.ts
+++ src/controllers/internal/get-latest-firmware-data.ts
@@ -1,11 +1,16 @@
+import { isNull } from "lodash"
 import { Response, Request } from "express"
 import EspLatestFirmwareManager from "../../classes/esp32/esp-latest-firmware-manager"
 
-export default function readCurrentFirmwareVersion(_req: Request, res: Response): void {
+export default function getLatestFirmwareData(_req: Request, res: Response): void {
 	try {
 		const currentFirmwareVersion = EspLatestFirmwareManager.getInstance().latestFirmwareVersion
+		const isBinaryNull = isNull(EspLatestFirmwareManager.getInstance().latestBinary)
 
-		res.status(200).json({ currentFirmwareVersion })
+		res.status(200).json({
+			currentFirmwareVersion,
+			isBinaryNull
+		})
 		return
 	} catch (error) {
 		console.error(error)
--- src/routes/internal-routes.ts
+++ src/routes/internal-routes.ts
@@ -7,7 +7,7 @@ import forceFirmwareRefetch from "../controllers/internal/force-firmware-refetch
 import addNewPipUUID from "../controllers/internal/add-new-pip-uuid"
 import decodeEmailSubscriber from "../controllers/internal/decode-email-subscriber"
 import displayLedColorsDirectly from "../controllers/internal/display-led-colors-directly"
-import readCurrentFirmwareVersion from "../controllers/internal/read-current-firmware-version"
+import getLatestFirmwareData from "../controllers/internal/get-latest-firmware-data"
 
 const internalRoutes = express.Router()
 
@@ -21,6 +21,6 @@ internalRoutes.post("/display-led-colors", displayLedColorsDirectly)
 
 internalRoutes.post("/firmware-update", forceFirmwareRefetch)
 
-internalRoutes.post("/read-current-firmware-version", readCurrentFirmwareVersion)
+internalRoutes.post("/get-latest-firmware-data", getLatestFirmwareData)
 
 export default internalRoutes
