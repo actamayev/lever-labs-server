2db68a7ff049f81c2b9a72e3a64abce8ec6c09d2
atamayev <bluedotrobots@gmail.com>
2025-05-30 11:37:55 -0400

    Update esp32-socket-manager.ts

--- src/classes/esp32/esp32-socket-manager.ts
+++ src/classes/esp32/esp32-socket-manager.ts
@@ -139,7 +139,6 @@ export default class Esp32SocketManager extends Singleton {
 			connection
 		})
 		this.socketToPip.set(socketId, pipUUID)
-		console.log(`ESP32 Connected: socketId: ${socketId} (PipID ${pipUUID})`)
 
 		// Notify of status change
 		BrowserSocketManager.getInstance().emitPipStatusUpdate(pipUUID, "online")
