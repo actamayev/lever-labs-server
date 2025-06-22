79db5b2f70bfe5b8b2bcc999873d3e95ce838b26
Ariel Tamayev <actamayev@gmail.com>
2025-06-22 10:37:07 -0400

    XML --> JSON (#60)
    * Xml --> Json
    
    * Update package-lock.json
    
    * Json
    
    * Re-name blockly key

--- src/controllers/sandbox/edit-sandbox-project.ts
+++ src/controllers/sandbox/edit-sandbox-project.ts
@@ -1,12 +1,13 @@
 import { Response, Request } from "express"
 import updateSandboxProject from "../../db-operations/write/sandbox-project/update-sandbox-project"
-import { ErrorResponse, SuccessResponse} from "@bluedotrobots/common-ts"
+import { BlocklyJson, ErrorResponse, SuccessResponse} from "@bluedotrobots/common-ts"
+
 export default async function editSandboxProject(req: Request, res: Response): Promise<void> {
 	try {
 		const { sandboxProjectId } = req
-		const { newXml } = req.body as { newXml: string }
+		const { newBlocklyJson } = req.body as { newBlocklyJson: BlocklyJson }
 
-		await updateSandboxProject(sandboxProjectId, newXml)
+		await updateSandboxProject(sandboxProjectId, newBlocklyJson)
 
 		res.status(200).json({ success: "" } as SuccessResponse)
 		return
