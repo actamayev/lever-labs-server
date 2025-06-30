fc019c0a56412e6cb302f8f68b996e345a499ed8
atamayev <bluedotrobots@gmail.com>
2025-06-30 13:14:32 -0400

    Get career quest chat

--- src/routes/chat-routes.ts
+++ src/routes/chat-routes.ts
@@ -1,13 +1,15 @@
 import express from "express"
 
 import jwtVerifyAttachUserId from "../middleware/jwt/jwt-verify-attach-user-id"
-import validateStreamId from "../middleware/request-validation/career-quest/validate-stream-id"
-import validateSendCareerQuestMessage from "../middleware/request-validation/career-quest/validate-send-career-quest-message"
+import attachCareerQuestChatId from "../middleware/attach/attach-career-quest-chat-id"
+import validateStreamId from "../middleware/request-validation/chat/validate-stream-id"
+import attachConversationHistory from "../middleware/attach/attach-conversation-history"
+import validateGetCareerQuestChat from "../middleware/request-validation/chat/validate-get-career-quest-chat"
+import validateSendCareerQuestMessage from "../middleware/request-validation/chat/validate-send-career-quest-message"
 
 import stopChatbotStream from "../controllers/chat/stop-chat-stream"
+import getCareerQuestChat from "../controllers/chat/get-career-quest-chat"
 import sendCareerQuestMessage from "../controllers/chat/send-career-quest-message"
-import attachCareerQuestChatId from "../middleware/attach/attach-career-quest-chat-id"
-import attachConversationHistory from "../middleware/attach/attach-conversation-history"
 
 const chatRoutes = express.Router()
 
@@ -25,6 +27,13 @@ chatRoutes.post(
 	jwtVerifyAttachUserId,
 	validateStreamId,
 	stopChatbotStream
+)
+
+chatRoutes.get(
+	"/career-quest-chat/:challengeId",
+	jwtVerifyAttachUserId,
+	validateGetCareerQuestChat,
+	getCareerQuestChat
 )
 
 export default chatRoutes
