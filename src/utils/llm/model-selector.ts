f07632b001df3edf17ece22862fb14a18d8a0809
atamayev <bluedotrobots@gmail.com>
2025-07-02 14:43:41 -0400

    Update common

--- src/utils/llm/model-selector.ts
+++ src/utils/llm/model-selector.ts
@@ -3,12 +3,12 @@ import { InteractionType } from "@bluedotrobots/common-ts"
 export default function selectModel(interactionType: InteractionType): string {
 	switch (interactionType) {
 	case "checkCode":
-		return "gemini-2.5-flash-lite-preview-06-17"
+		return "google/gemini-2.5-flash-lite-preview-06-17"
 	case "hint":
-		return "gemini-2.5-flash-lite-preview-06-17"
+		return "google/gemini-2.5-flash-lite-preview-06-17"
 	case "generalQuestion":
-		return "gemini-2.5-flash-lite-preview-06-17"
+		return "google/gemini-2.5-flash-lite-preview-06-17"
 	default:
-		return "gemini-2.5-flash-lite-preview-06-17"
+		return "google/gemini-2.5-flash-lite-preview-06-17"
 	}
 }
