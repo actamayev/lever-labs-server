c56fe64abcf564a0bbe1e5fbf6e371e2de7af219
atamayev <bluedotrobots@gmail.com>
2025-07-06 16:13:14 -0400

    Update send-career-quest-message.ts

--- src/controllers/chat/send-career-quest-message.ts
+++ src/controllers/chat/send-career-quest-message.ts
@@ -98,7 +98,7 @@ async function processLLMRequest(
 			})),
 			stream: true,
 			temperature: 0.7,
-			max_completion_tokens: 5000
+			max_completion_tokens: 3500
 		}, {
 			signal: abortSignal
 		})
