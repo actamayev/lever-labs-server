9868b0c0f6faaf43f724da76775a7cbc3e9865a0
atamayev <bluedotrobots@gmail.com>
2025-07-06 16:04:00 -0400

    Cleanup

--- src/controllers/chat/send-career-quest-message.ts
+++ src/controllers/chat/send-career-quest-message.ts
@@ -98,7 +98,7 @@ async function processLLMRequest(
 			})),
 			stream: true,
 			temperature: 0.7,
-			max_completion_tokens: 1000
+			max_completion_tokens: 5000
 		}, {
 			signal: abortSignal
 		})
--- src/routes/career-quest-routes.ts
+++ src/routes/career-quest-routes.ts
@@ -1,9 +1,8 @@
 import express from "express"
 
 import jwtVerifyAttachUserId from "../middleware/jwt/jwt-verify-attach-user-id"
-import validateEditCareerQuestSandboxProject
-	from "../middleware/request-validation/career-quest/validate-edit-career-quest-sandbox-project"
 import validateChallengeIdInParams from "../middleware/request-validation/chat/validate-challenge-id-in-params"
+import validateEditSandboxProject from "../middleware/request-validation/sandbox/validate-edit-sandbox-project"
 
 import editCareerQuestSandboxProject from "../controllers/career-quest/edit-career-quest-sandbox-project"
 import retrieveCareerQuestChallengeData from "../controllers/career-quest/retrieve-career-quest-challenge-data"
@@ -13,7 +12,7 @@ const careerQuestRoutes = express.Router()
 careerQuestRoutes.post(
 	"/edit-career-quest-sandbox-project/:challengeId",
 	validateChallengeIdInParams,
-	validateEditCareerQuestSandboxProject,
+	validateEditSandboxProject,
 	jwtVerifyAttachUserId,
 	editCareerQuestSandboxProject
 )
