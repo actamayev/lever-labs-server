eb0876e8fe79364f326bbeb0f968ecb33407ba2c
atamayev <bluedotrobots@gmail.com>
2025-07-13 16:05:53 -0400

    Update generate-classroom-code.ts

--- src/utils/generate-classroom-code.ts
+++ src/utils/generate-classroom-code.ts
@@ -2,7 +2,7 @@ import { ClassCode } from "@bluedotrobots/common-ts"
 
 export default function generateClassroomCode(): ClassCode {
 	try {
-		const characters = "ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz23456789"
+		const characters = "abcdefghijkmnpqrstuvwxyz23456789"
 		let result = ""
 		for (let i = 0; i < 5; i++) {
 			const randomIndex = Math.floor(Math.random() * characters.length)
