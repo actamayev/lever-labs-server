3d2adcfc5572e37ab251d27fa96eb9569f905c9e
actamayev <bluedotrobots@gmail.com>
2025-09-21 16:47:16 -0400

    Cleanup

--- src/types/esp-socket.ts
+++ src/types/esp-socket.ts
@@ -13,6 +13,16 @@ declare global {
 	}
 
 	type DisconnectReason = "ping_timeout" | "ping_failed" | "socket_closed" | "socket_error" | "disposed"
+
+	interface ESPConnectionState {
+		online: boolean                 // WebSocket connection active
+		connectedToOnlineUserId: number | null   // UserId of the current user connected to the PIP via Browser
+		connectedToSerialUserId: number | null      // UserId of the user currently connected to the PIP via USB/serial
+		lastOnlineConnectedUser: {
+			userId: number
+			connectedAt: Date
+		} | null
+	}
 }
 
 export {}
