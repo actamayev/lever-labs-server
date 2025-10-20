7d7a7d3f9745d6b55634dce275fee7079f5e8d2f
lever-labs <hello@leverlabs.com>
2025-10-20 18:30:00 -0400

    Package update

--- sql/selects.sql
+++ sql/selects.sql
@@ -8,7 +8,7 @@
 -- SELECT * FROM challenge ORDER BY created_at;
 -- SELECT * FROM sandbox_project ORDER BY updated_at;
 
--- SELECT * FROM pip_uuid ORDER BY pip_uuid_id;
+SELECT * FROM pip_uuid;
 
 -- SELECT *
 -- FROM pip_uuid
@@ -34,6 +34,6 @@
 -- SELECT * FROM fill_in_the_blank;
 -- SELECT * FROM fill_in_the_blank_block_bank ORDER BY fill_in_the_blank_block_bank_id;
 -- SELECT * FROM fill_in_the_blank_user_answer;
-SELECT * FROM sandbox_project
-WHERE sandbox_project::text LIKE '%BACKWARD%'
-ORDER BY created_at;
+-- SELECT * FROM challenge_sandbox
+-- WHERE challenge_sandbox_json::text LIKE '%backward%'
+-- ORDER BY created_at;
