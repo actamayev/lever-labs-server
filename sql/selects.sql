ed6c8c482bf4cebb18e94ea97589a414c1c9dac8
lever-labs <hello@leverlabs.com>
2025-10-21 11:33:15 -0400

    Update selects.sql

--- sql/selects.sql
+++ sql/selects.sql
@@ -34,7 +34,7 @@
 
 -- SELECT * FROM block_to_function_flashcard;
 -- SELECT * FROM function_to_block_flashcard;
--- SELECT * FROM fill_in_the_blank;
+SELECT * FROM fill_in_the_blank;
 -- SELECT * FROM fill_in_the_blank_block_bank ORDER BY fill_in_the_blank_block_bank_id;
 -- SELECT * FROM fill_in_the_blank_user_answer;
 -- SELECT * FROM challenge_sandbox
